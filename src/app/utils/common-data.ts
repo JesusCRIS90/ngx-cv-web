19ca7ca7b54f9aaffb7c429d8f95032177720d82
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-17 19:05:31 +0200

    Fix DATA_PATH to use relative path for app data JSON

--- src/app/utils/common-data.ts
+++ src/app/utils/common-data.ts
@@ -1,4 +1,4 @@
 export const SVGs_PATH = '/assets/sprite-sheet/icons-sheet.svg';
-export const DATA_PATH = '/assets/data/app-data.json';
+export const DATA_PATH = '/data/app-data.json';
 
 export const DATA_KEY = "AppKey";
