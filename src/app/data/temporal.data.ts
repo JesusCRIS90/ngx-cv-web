4dbbe9a130c1d607c6a97d8a46153aa023f57728
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-27 13:41:45 +0200

    Add NavMenuItems data structure for navigation menu

---
+++ src/app/data/temporal.data.ts
@@ -0,0 +1,39 @@
+import { NavItem } from "../components"
+
+export const NavMenuItems: NavItem[] = [
+  {
+    icon: 'icon-monitor',
+    id: 'home',
+    navAction: null,
+    title: null,
+    subtitle: null
+  },
+  {
+    icon: 'icon-monitor',
+    id: 'projects',
+    navAction: null,
+    title: null,
+    subtitle: null
+  },
+  {
+    icon: 'icon-monitor',
+    id: 'experience',
+    navAction: null,
+    title: null,
+    subtitle: null
+  },
+  {
+    icon: 'icon-monitor',
+    id: 'skills',
+    navAction: null,
+    title: null,
+    subtitle: null
+  },
+    {
+    icon: 'icon-monitor',
+    id: 'contact',
+    navAction: null,
+    title: null,
+    subtitle: null
+  },
+]
