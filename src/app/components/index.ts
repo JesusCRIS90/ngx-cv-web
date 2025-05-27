b7a91a98c950d667f2b2bdbc61436035d47f5e92
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-27 13:41:02 +0200

    Add exports for nav-item and nav-sec-menu components and interfaces

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,1 +1,4 @@
 export * from "./scroll-tracker/scroll-tracker.component"
+export * from "./nav-item/nav-item.component"
+export * from "./nav-item/nav-item.interface"
+export * from "./nav-sec-menu/nav-sec-menu.component"
