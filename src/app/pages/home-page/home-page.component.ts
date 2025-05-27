0aeee7fea8516b4c24dec54af2d5e1c4bc7fe2ec
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-27 13:41:34 +0200

    Add navigation menu component and implement click handling for smooth scrolling

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, signal, ViewChildren } from '@angular/core';
 
-import { ScrollTrackerComponent, ScrollYEventInfo } from '../../components'
+import { ScrollTrackerComponent, ScrollYEventInfo, NavSecMenuComponent, NavItem } from '../../components'
 import { SectionTrackerDirective } from "../../directives"
 
 import {
@@ -10,6 +10,9 @@ import {
   ProjectsSectionComponent,
   SkillsSectionComponent
 } from "../../sections"
+
+// Delete after
+import { NavMenuItems } from "../../data/temporal.data"
 
 @Component({
   selector: 'app-home-page',
@@ -21,19 +24,41 @@ import {
     HomeSectionComponent,
     ProjectsSectionComponent,
     SkillsSectionComponent,
-    SectionTrackerDirective
+    SectionTrackerDirective,
+    NavSecMenuComponent,
   ],
   templateUrl: './home-page.component.html',
 })
-export default class HomePageComponent {
+export default class HomePageComponent implements AfterViewInit {
+
+  temNavItems: NavItem[] = NavMenuItems;
+  activeSecId = signal<string>('home');
+
+  @ViewChildren(SectionTrackerDirective) sections!: QueryList<SectionTrackerDirective>;
+
   yScrollEventData = signal<ScrollYEventInfo>({ direction: 'up', yPosPercentage: 0, yPosPixel: 0, yMaxScrollSizePixel: 0 })
 
+  ngAfterViewInit(): void {}
+
   getYScrollEvent(scrollEvent: ScrollYEventInfo) {
-    // console.log(scrollEvent);
-    this.yScrollEventData.set( scrollEvent );
+    this.yScrollEventData.set(scrollEvent);
   }
 
-  onSectionActive( sectionID: string ){
-    console.log( sectionID );
+  onSectionActive(sectionID: string) {
+    console.log(sectionID);
+    this.activeSecId.set(sectionID);
   }
+
+  protected onClickNavMenu(secId: string) {
+
+    console.log(secId);
+
+    this.sections.forEach((section, index) => {
+      console.log(`Section ${index}:`, section.getSectionId());
+      if (section.getSectionId() === secId ) {
+        section.el.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  }
+
 }
