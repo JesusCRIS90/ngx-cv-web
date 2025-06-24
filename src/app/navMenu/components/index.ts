ac47d4af98a28dbb400d73ec37d475f31015875d
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-06-24 20:10:45 +0200

    Remove NavItemComponent and its associated template; update index export to comment out NavItemComponent

--- src/app/navMenu/components/index.ts
+++ src/app/navMenu/components/index.ts
@@ -1,4 +1,4 @@
-export * from './nav-item/nav-item.component'
+// export * from './nav-item/nav-item.component'
 export * from './hori-nav-sec-menu/hori-nav-sec-menu.component'
 export * from './scroll-tracker/scroll-tracker.component'
 export * from './vert-nav-sec-menu/vert-nav-sec-menu.component'
