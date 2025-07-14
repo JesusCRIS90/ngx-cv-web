36548d037be010969264c8253806bbda20849f13
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-07-14 13:27:02 +0200

    [REFACTOR] Replace provideColorSchema with provideBeeColorSchema in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,17 +5,18 @@ import {
   StoragesManager
 } from "@beexy/tools"
 
+import { provideBeeColorSchema } from '@beexy/ngx-providers'
+
 import { routes } from './app.routes';
 
 import { COMMON_APP_CONFIG_PROVIDER } from './providers/config'
-import { provideColorSchema } from './providers/color'
 import { SPRITE_SHEET_PROVIDER, JSON_DATA_PROVIDER } from './providers/pre-loaders'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideColorSchema({
+    provideBeeColorSchema({
       availableSchemas: ['violet', 'blue'],
       prefix: 'color-schema-',
       initialSchema: 'violet'
