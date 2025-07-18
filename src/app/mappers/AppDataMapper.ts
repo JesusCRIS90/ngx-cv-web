4fc40647b21071d81ffa2fd582cab56862303422
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-07-18 12:51:32 +0200

    [UPDATE] Enhance normalizeHomeData method to improve data extraction and handling

--- src/app/mappers/AppDataMapper.ts
+++ src/app/mappers/AppDataMapper.ts
@@ -10,20 +10,48 @@ import {
 export class AppDataMapper {
   public static normalizeData(jsonData: unknown): AppData {
     const data: AppData = {
-      home:       AppDataMapper.normalizeHomeData(jsonData),
-      projects:   AppDataMapper.normalizeProjectsData(jsonData),
+      home: AppDataMapper.normalizeHomeData(jsonData),
+      projects: AppDataMapper.normalizeProjectsData(jsonData),
       experience: AppDataMapper.normalizeExperienceData(jsonData),
-      skills:     AppDataMapper.normalizeSkillsData(jsonData),
-      contact:    AppDataMapper.normalizeContactData(jsonData),
+      skills: AppDataMapper.normalizeSkillsData(jsonData),
+      contact: AppDataMapper.normalizeContactData(jsonData),
     };
 
     return data;
   }
 
   private static normalizeHomeData(jsonData: unknown): AppDataHome {
-    const data: AppDataHome = {}
 
-    // TODO: Here the respective adaptation
+    const dataObj = typeof jsonData === 'object' && jsonData !== null ? jsonData as any : {};
+
+    // console.log( "Home Data Adaptation", dataObj )
+
+    const data: AppDataHome = {
+      name: {
+        text: dataObj?.Home?.name?.text ?? 'No Defined',
+        keywords: dataObj.Home?.name?.keywords ?? [],
+      },
+      role: {
+        text: dataObj?.Home?.role?.text ?? 'No Defined',
+        keywords: dataObj?.Home?.role?.keywords ?? [],
+      },
+      briefBio: {
+        text: dataObj?.Home?.briefBio?.text ?? 'No Defined',
+        keywords: dataObj?.Home?.briefBio?.keywords ?? [],
+      },
+      profileImg: {
+        vert: dataObj?.Home?.profileImage?.vertical ?? '',
+        hori: dataObj?.Home?.profileImage?.horizontal ?? '',
+      },
+      background: {
+        vert: dataObj?.Home?.background?.vertical ?? '',
+        hori: dataObj?.Home?.background?.horizontal ?? '',
+      },
+      url_linkedin: dataObj?.Home?.linkedin_url ?? '',
+      url_github: dataObj?.Home?.github_url ?? '',
+      url_downloadCV: dataObj?.Home?.downloable_cv_url ?? '',
+      url_email: dataObj?.Home?.da_email ?? '',
+    }
 
     return data;
   }
