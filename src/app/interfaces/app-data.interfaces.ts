7d9b3431481d53ab623ff642ef811891141d3e95
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-07-18 12:50:34 +0200

    [UPDATE] Define properties for AppDataHome interface to structure user data

--- src/app/interfaces/app-data.interfaces.ts
+++ src/app/interfaces/app-data.interfaces.ts
@@ -7,8 +7,29 @@ export interface AppData {
 }
 
 // TODO: Add the necesary data on each interface
-export interface AppDataHome{}
+export interface AppDataHome{
+  name: TextWithKeyWords,
+  role: TextWithKeyWords,
+  briefBio: TextWithKeyWords,
+  profileImg: ImageResponsiveVersions,
+  background: ImageResponsiveVersions,
+  url_linkedin: string,
+  url_github: string,
+  url_downloadCV: string,
+  url_email: string,
+}
 export interface AppDataProjects{}
 export interface AppDataExperience{}
 export interface AppDataSkills{}
 export interface AppDataContact{}
+
+
+interface TextWithKeyWords {
+  text: string,
+  keywords: string[]
+}
+
+interface ImageResponsiveVersions {
+  hori: string,
+  vert: string,
+}
