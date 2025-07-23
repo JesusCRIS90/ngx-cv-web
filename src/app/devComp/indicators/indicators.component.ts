83cd1306702c288ff37655e20c9dafd726121565
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-07-23 18:03:26 +0200

    [MOD] Update IndicatorsComponent

--- src/app/devComp/indicators/indicators.component.ts
+++ src/app/devComp/indicators/indicators.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, ContentChild, input, TemplateRef } from '@angular/core';
+import { Component, computed, ContentChild, EventEmitter, input, Output, TemplateRef } from '@angular/core';
 import { NgTemplateOutlet, CommonModule } from '@angular/common';
 
 
@@ -12,12 +12,18 @@ import { NgTemplateOutlet, CommonModule } from '@angular/common';
 export class IndicatorsComponent {
 
   idx!: number;
+
   totalElements = input.required<number>();
   activeElement = input<number>(0);
   activeElementClass = input<string>('');
+
+  @Output() indexClicked = new EventEmitter<number>();
 
   @ContentChild('element') element!: TemplateRef<{ isActive: boolean }>;
 
   range = computed(() => Array.from({ length: this.totalElements() }, (_, i) => i));
 
+  click(index: number) {
+    this.indexClicked.emit(index);
+  }
 }
