37c8ae012cda7b58db0071f6af7ac07cc6487540
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-09 19:44:53 +0200

    [MOD] Update AppComponent and ProjectSectionComponent

--- src/app/sections/project-section/project-section.component.ts
+++ src/app/sections/project-section/project-section.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
+
+import {
+  ResponsiveLayoutComponent as ResponsiveLayout,
+  GridLayout2DComponent as Grid2D,
+  ItemGridLayoutComponent as ItemGrid,
+} from '@beexy/ngx-layouts'
+
+// TODO: Remove on a near Future
 const horiUrls = [
   "./assets/images/hori-test-1.jpg",
   "./assets/images/hori-test-2.jpg",
@@ -12,6 +20,9 @@ const vertUrls = [
   "./assets/images/dog-test-3.jpg",
 ]
 
+
+
+
 import {
   ShortVertProjectCardComponent as ShortVertCardProject,
   ShortHoriProjectCardComponent as ShortHoriCardProject,
@@ -22,9 +33,11 @@ import {
   BeeVerticalCarouselComponent as VertCarousel,
 } from '@beexy/ngx-navigation'
 
+import { APP_COMMON_CONFIG_TOKEN, AppCommonConfig } from '../../providers/config';
+
 @Component({
   selector: 'sec-projects',
-  imports: [ShortVertCardProject, ShortHoriCardProject, HoriCarousel, VertCarousel],
+  imports: [ResponsiveLayout, Grid2D, ItemGrid, ShortVertCardProject, ShortHoriCardProject, HoriCarousel, VertCarousel],
   templateUrl: './project-section.component.html',
   styleUrl: './project-section.component.css'
 })
@@ -33,4 +46,10 @@ export class ProjectsSectionComponent {
   horiURLs = horiUrls;
   vertURLs = vertUrls;
 
+  private commonConfig: AppCommonConfig = inject(APP_COMMON_CONFIG_TOKEN);
+
+
+  protected getFactorVert2Hori(): number {
+    return this.commonConfig.factorVert2Hori;
+  }
 }
