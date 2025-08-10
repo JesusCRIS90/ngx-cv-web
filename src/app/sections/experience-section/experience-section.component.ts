03ec4333a373dc8143a07a803cd6e6fbff111c10
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-10 23:03:47 +0200

    [MOD] Update Experience Section Component

--- src/app/sections/experience-section/experience-section.component.ts
+++ src/app/sections/experience-section/experience-section.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+import { TimelineModule } from 'primeng/timeline';
+
 import { ShortExperienceCardComponent } from '../../components'
+
+import { TimeLineCard } from '../../interfaces'
+import { TimelineComponent } from '../../devComp/timeline/timeline.component'
+
+const experinceArray: TimeLineCard[] = [
+  {
+    icon: 'bicon-tech-education',
+    role: 'Software Engineer',
+    company: 'Seville University',
+    timeSpan: 'ENE 2019 - JUL 2024',
+    description: 'Brief Description'
+  },
+  {
+    icon: 'bicon-tech-education',
+    role: 'Software Engineer',
+    company: 'Seville University',
+    timeSpan: 'ENE 2019 - JUL 2024',
+    description: 'Brief Description'
+  },
+  {
+    icon: 'bicon-tech-education',
+    role: 'Software Engineer',
+    company: 'Seville University',
+    timeSpan: 'ENE 2019 - JUL 2024',
+    description: 'Brief Description'
+  }
+]
 
 @Component({
   selector: 'sec-experience',
-  imports: [ShortExperienceCardComponent],
+  imports: [ShortExperienceCardComponent, TimelineComponent],
   templateUrl: './experience-section.component.html',
 })
-export class ExperienceSectionComponent { }
+export class ExperienceSectionComponent {
+
+  events: TimeLineCard[] = experinceArray;
+
+  getEvents(): TimeLineCard[] {
+    return this.events;
+  }
+}
