70a90013b0342ebbb7ed636e139dde914c8c7cce
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-10 23:02:55 +0200

    [MOD] Fix minor Issues

--- src/app/components/short-experience-card/short-experience-card.component.ts
+++ src/app/components/short-experience-card/short-experience-card.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import {
   PairLayoutComponent as PairLay,
   PAIR_DISTRIBUTION as PAIR_POLICY,
 } from '@beexy/ngx-layouts'
+
+import { TimeLineCard } from '../../interfaces'
 
 @Component({
   selector: 'short-experience-card',
@@ -13,4 +15,12 @@ import {
 })
 export class ShortExperienceCardComponent {
   PAIR_POLICY = PAIR_POLICY;
+
+  experience = input.required<TimeLineCard>();
+
+  getRole(): string { return this.experience().role }
+  getTimeSpan(): string { return this.experience().timeSpan }
+  getCompany(): string { return this.experience().company }
+  getDescription(): string { return this.experience().description }
+
 }
