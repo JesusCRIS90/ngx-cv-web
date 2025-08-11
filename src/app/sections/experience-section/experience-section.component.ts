3c3d010017ad460eff2143adcbdf11a1cafb6644
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-11 14:05:09 +0200

    [MOD] Updated globals styles file and Improve Experience Section Component

--- src/app/sections/experience-section/experience-section.component.ts
+++ src/app/sections/experience-section/experience-section.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
-import { TimelineModule } from 'primeng/timeline';
+import {
+  ResponsiveLayoutComponent as ResponsiveLayout,
+  ScreenOrientation,
+} from '@beexy/ngx-layouts'
 
-import { ShortExperienceCardComponent } from '../../components'
+import {
+  RoundIconComponent as SVGRound,
+} from '@beexy/ngx-components'
+
+import { NoModalWindowService } from '@beexy/ngx-popups'
+
+import { ShortExperienceCardComponent, LongExperienceCardComponent } from '../../components'
+import { ClickableActionDirective } from '../../directives'
 
 import { TimeLineCard } from '../../interfaces'
-import { TimelineComponent } from '../../devComp/timeline/timeline.component'
+import { TimelineComponent, TimelineAlign } from '../../devComp/timeline/timeline.component'
+
+import { APP_COMMON_CONFIG_TOKEN, AppCommonConfig } from '../../providers/config';
+
 
 const experinceArray: TimeLineCard[] = [
   {
@@ -13,34 +26,71 @@ const experinceArray: TimeLineCard[] = [
     role: 'Software Engineer',
     company: 'Seville University',
     timeSpan: 'ENE 2019 - JUL 2024',
-    description: 'Brief Description'
+    description: 'Brief Description',
+    type: 'experience',
   },
   {
     icon: 'bicon-tech-education',
     role: 'Software Engineer',
     company: 'Seville University',
     timeSpan: 'ENE 2019 - JUL 2024',
-    description: 'Brief Description'
+    description: 'Brief Description',
+    type: 'experience',
   },
   {
     icon: 'bicon-tech-education',
     role: 'Software Engineer',
     company: 'Seville University',
     timeSpan: 'ENE 2019 - JUL 2024',
-    description: 'Brief Description'
+    description: 'Brief Description',
+    type: 'education',
   }
 ]
 
 @Component({
   selector: 'sec-experience',
-  imports: [ShortExperienceCardComponent, TimelineComponent],
+  imports: [ShortExperienceCardComponent, TimelineComponent, SVGRound, ClickableActionDirective, ResponsiveLayout],
   templateUrl: './experience-section.component.html',
 })
 export class ExperienceSectionComponent {
 
   events: TimeLineCard[] = experinceArray;
+  timelineAligment = signal<TimelineAlign>('alternate');
+
+  private commonConfig: AppCommonConfig = inject(APP_COMMON_CONFIG_TOKEN);
+
+  constructor(private noModalService: NoModalWindowService) { }
 
   getEvents(): TimeLineCard[] {
     return this.events;
+  }
+
+  getIconRef(type: 'education' | 'experience') {
+    if (type === 'education') return 'bicon-common-education';
+    else return 'bicon-common-experience';
+  }
+
+  onClickExpCard() {
+    console.log('Experience Card Clicked')
+    this.noModalService.open({
+      component: LongExperienceCardComponent,
+      data: {}
+    })
+  }
+
+  protected getFactorVert2Hori(): number {
+    return this.commonConfig.factorVert2Hori;
+  }
+
+  protected getTimeLineAligment(): TimelineAlign{
+    return this.timelineAligment();
+  }
+
+  protected onOrientationChange(orientation: ScreenOrientation){
+    if( orientation === 'landscape' ){
+      this.timelineAligment.set( 'alternate' );
+      return;
+    }
+    this.timelineAligment.set( 'right' );
   }
 }
