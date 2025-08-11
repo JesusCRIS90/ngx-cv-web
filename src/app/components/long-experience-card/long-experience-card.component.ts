35050511996b4a167537f53260c4785a7244097f
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-11 14:04:13 +0200

    [ADD] New Component - Long-Experience-Card

---
+++ src/app/components/long-experience-card/long-experience-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-long-experience-card',
+  imports: [],
+  templateUrl: './long-experience-card.component.html',
+  styleUrl: './long-experience-card.component.css',
+})
+export class LongExperienceCardComponent { }
