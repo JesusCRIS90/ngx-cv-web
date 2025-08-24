5bd1a91b053df542edf8b21b5918cca3275a461a
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-08-24 19:02:09 +0200

    [MOD] Update appConfig - Responsiveness Schemmas Updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,25 @@ import {
   StoragesManager
 } from "@beexy/tools"
 
-import { provideBeeColorSchema, provideBeeResponsiveSchema } from '@beexy/ngx-providers'
+import { provideBeeColorSchema, provideBeeResponsiveSchema, defaultResponsivenessTaxonomy2DMap } from '@beexy/ngx-providers'
 
 import { routes } from './app.routes';
 
-import { USER_CONFIG_APP_PROVIDER  } from './providers/config'
+import { USER_CONFIG_APP_PROVIDER } from './providers/config'
 import { SPRITE_SHEET_PROVIDER, JSON_DATA_PROVIDER } from './providers/pre-loaders'
 
+const responsiveAvailablesSchemmas = [
+  'small-screen',
+  'medium-screen',
+  'big-screen',
+  'extra-wide-screen',
+  'ultra-wide-screen',
+  'smartphone-vertical',
+  'big-smartphone-vertical',
+  'small-tablet-vertical',
+  'tablet-vertical',
+  'big-tablet-vertical'
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,9 +35,10 @@ export const appConfig: ApplicationConfig = {
       initialSchema: 'violet'
     }),
     provideBeeResponsiveSchema({
-      availableSchemas: ['big-screen', 'tablets', 'smartphone'],
+      availableSchemas: responsiveAvailablesSchemmas,
       prefix: 'responsive-schema-',
-      debouncingDelayMS: 100
+      debouncingDelayMS: 100,
+      responsiveTaxonomyMap: defaultResponsivenessTaxonomy2DMap
     }),
     StoragesManager,
     USER_CONFIG_APP_PROVIDER,
