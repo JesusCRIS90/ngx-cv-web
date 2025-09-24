e9a68ccbc4c2eba6e55ec00812ba566243ed370a
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-09-24 20:12:14 +0200

    [MOD] Update Contact Me Section

--- src/app/sections/contact-section/contact-section.component.ts
+++ src/app/sections/contact-section/contact-section.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'sec-contact',
   imports: [],
   templateUrl: './contact-section.component.html',
 })
-export class ContactSectionComponent { }
+export class ContactSectionComponent {
+
+  @ViewChild('gifElement') gifElement!: ElementRef<HTMLImageElement>;
+  gifLoaded = false;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        this.gifLoaded = true;
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(this.gifElement.nativeElement);
+  }
+
+  getGIF(): string {
+    return "https://raw.githubusercontent.com/JesusCRIS90/jc-risquez-cdn/main/jriz-cv/gifs/Building1.gif"
+  }
+
+}
