5d8fbfc62ecd1f232c08923f4e629b8fc1362425
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-09-25 18:21:12 +0200

    [MOD] Update Contact Section

--- src/app/sections/contact-section/contact-section.component.ts
+++ src/app/sections/contact-section/contact-section.component.ts
@@ -1,14 +1,40 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, ViewChild } from '@angular/core';
+
+import {
+  HorizontalLayoutComponent as FlexHori,
+} from '@beexy/ngx-layouts'
+import {
+  HighlightKeyWordsDirective as keywordDir,
+  LinkIconComponent as LinkIcon,
+  SVGIconComponent as SVGIcon
+} from '@beexy/ngx-components'
+
+import { ToastService } from '@beexy/ngx-popups'
+
+import {
+  ClickableActionDirective as ActionClickDir
+} from '../../directives'
+
+import { SampleToastComponent } from '../../components'
+import { AppDataHome } from '../../interfaces';
+import { copyToClipboard } from '../../utils'
+
 
 @Component({
   selector: 'sec-contact',
-  imports: [],
+  imports: [FlexHori, LinkIcon, SVGIcon, ActionClickDir, keywordDir],
   templateUrl: './contact-section.component.html',
 })
-export class ContactSectionComponent {
+export class ContactSectionComponent implements AfterViewInit {
+
+  data = input.required<AppDataHome|undefined>();
 
   @ViewChild('gifElement') gifElement!: ElementRef<HTMLImageElement>;
   gifLoaded = false;
+
+  constructor(
+    private toastService: ToastService
+  ) { }
 
   ngAfterViewInit() {
     const observer = new IntersectionObserver(entries => {
@@ -21,8 +47,28 @@ export class ContactSectionComponent {
     observer.observe(this.gifElement.nativeElement);
   }
 
+  getData(): AppDataHome {
+    return this.data()!;
+  }
+
   getGIF(): string {
     return "https://raw.githubusercontent.com/JesusCRIS90/jc-risquez-cdn/main/jriz-cv/gifs/Building1.gif"
+  }
+
+  protected async copyEmailtToClipboard(datEmited: void) {
+    let message = 'Email Copied to Clipboard';
+
+    const success = await copyToClipboard(this.data()!.url_email);
+    if (!success) {
+      message = 'Error Copying email to clipboar. Try again'
+    }
+
+    this.toastService.open({
+      component: SampleToastComponent,
+      data: {
+        message
+      }
+    })
   }
 
 }
