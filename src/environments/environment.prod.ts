aa3bda45a2507e690fd2a4c48acb287cd69fccd7
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-12-12 17:00:27 +0100

    [MOD] Update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: true,
-    baseHref: '/your-github-repo-name-here' // <-- Match your GitHub repo name
+  production: true,
+  baseHref: '/your-github-repo-name-here',
+  formsparkId: 'fHTzX5VOC',
+  web3formPubKey: 'b0a62016-7aa0-40cf-8be8-a80c161eccd9',
+  web3FormHCaptchaCommonKey: '50b2fe65-b00b-4b9e-ad62-3ba471098be2',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
     production: false,
-    baseHref: ''
+    baseHref: '',
+    formsparkId: '',
+    web3formPubKey: 'b0a62016-7aa0-40cf-8be8-a80c161eccd9',
+    web3FormHCaptchaCommonKey: '50b2fe65-b00b-4b9e-ad62-3ba471098be2'
 };
