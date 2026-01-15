74e8869c824af99379d2cc521df3728f1ae3ca50
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2026-01-15 18:54:26 +0100

    [ADD] Enhance article and blog list pages with dynamic content loading and improved structure

--- src/app/pages/article-page/article-page.ts
+++ src/app/pages/article-page/article-page.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BlogArticlesService } from '../../services';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-article-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './article-page.html',
-  styleUrls: ['./article-page.css']
+  styleUrls: ['./article-page.css'],
 })
-export default class ArticlePageComponent {
-
-  slug:string|null = null;
+export default class ArticlePageComponent implements OnInit {
+  slug: string | null = null;
   
+  // Store the whole article object
+  article = signal<Record<string, any> | null>(null);
+
+  private blogService = inject(BlogArticlesService);
+
   constructor(private route: ActivatedRoute) {
     this.slug = this.route.snapshot.paramMap.get('slug');
     console.log('Article slug:', this.slug);
+  }
+
+  ngOnInit(): void {
+    this.onSlugSeach(this.slug ?? '');
+  }
+
+  async onSlugSeach(slug: string) {
+    const { data } = await this.blogService.getBySlug(slug);
+    this.article.set(data ?? null);
+
+    console.log('Article Data:', this.article());
   }
 }
--- src/app/pages/blog-list-page/blog-list-page.ts
+++ src/app/pages/blog-list-page/blog-list-page.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+import { BlogArticlesService, BlogSearchFilters } from '../../services';
 
 @Component({
   selector: 'app-blog-list-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog-list-page.html',
-  styleUrls: ['./blog-list-page.css']
+  styleUrls: ['./blog-list-page.css'],
 })
-export default class BlogListPageComponent {}
+export default class BlogListPageComponent implements OnInit {
+  articles = signal<any[]>([]);
+
+  private blogService = inject(BlogArticlesService);
+
+  ngOnInit(): void {
+    this.onSearch({});
+  }
+
+  async onSearch(filters: BlogSearchFilters) {
+    const { data } = await this.blogService.search(filters);
+    this.articles.set(data ?? []);
+
+    console.log('Search results:', this.articles());
+  }
+
+  getArticlesNames(): string[] {
+    return this.articles().map(article => article.title);
+  }
+
+}
