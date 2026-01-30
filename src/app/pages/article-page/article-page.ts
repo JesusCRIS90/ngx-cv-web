99d528f62ba9b5b0bf4197c7ce72a40af898228e
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2026-01-30 19:28:40 +0100

    [ADD] Refactor article page with enhanced layout, dynamic content loading, and navigation improvements

--- src/app/pages/article-page/article-page.ts
+++ src/app/pages/article-page/article-page.ts
@@ -1,7 +1,16 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { BlogArticlesService } from '../../services';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JsonPipe } from '@angular/common';
+
+import { BlogArticlesService } from '../../services';
+
+import { POLICY_POSITION as ANCHOR_POLICY_POSITION } from '@beexy/ngx-layouts';
 
 import {
   MarkdownViewerComponent as MarkdownViewer,
@@ -9,47 +18,71 @@ import {
   MarkdownHeading,
 } from '@beexy/ngx-components';
 
+import { HeaderArticle } from '../../components';
+import { ArticleHeader } from '../../interfaces';
+import { AppInterfacesAdapter } from '../../mappers';
+
 type ArticleLanguage = 'en' | 'es';
 
 @Component({
   selector: 'app-article-page',
-  imports: [JsonPipe, MarkdownViewer, MarkdownNavigator],
+  imports: [JsonPipe, MarkdownViewer, MarkdownNavigator, HeaderArticle],
+  standalone: true,
   templateUrl: './article-page.html',
   styleUrls: ['./article-page.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export default class ArticlePageComponent implements OnInit {
+  ANCHOR_POLICY_POSITION = ANCHOR_POLICY_POSITION;
+
   slug: string | null = null;
-  
+
   // Store the whole article object
   article = signal<Record<string, any> | null>(null);
   headingsDocs = signal<MarkdownHeading[]>([]);
 
   private blogService = inject(BlogArticlesService);
-
-
-  onHeadingsChange(headings: MarkdownHeading[]): void {
-    console.log('Headings changed:', headings);
-    this.headingsDocs.set(headings);
-  }
-
-  constructor(private route: ActivatedRoute) {
+  
+  constructor(
+    private route: ActivatedRoute,
+    private readonly router: Router,
+  ) {
     this.slug = this.route.snapshot.paramMap.get('slug');
-    console.log('Article slug:', this.slug);
+    // console.log('Article slug:', this.slug);
   }
-
+  
   ngOnInit(): void {
     this.onSlugSeach(this.slug ?? '');
   }
 
+  onHeadingsChange(headings: MarkdownHeading[]): void {
+    // console.log('Headings changed:', headings);
+    this.headingsDocs.set(headings);
+  }
+  
   async onSlugSeach(slug: string) {
     const { data } = await this.blogService.getBySlug(slug);
     this.article.set(data ?? null);
 
-    console.log('Article Data:', this.article());
+    // console.log('Article Data:', this.article());
   }
 
-  protected markdownPath( lang: ArticleLanguage = 'en' ): string {
+  protected markdownPath(lang: ArticleLanguage = 'en'): string {
     const langKey = lang === 'en' ? 'markdown_en_url' : 'markdown_es_url';
     return this.article()?.[langKey] ?? '';
+  }
+
+  protected get articleHeader(): ArticleHeader {
+    let articleData = this.article();
+
+    if (!articleData) {
+      articleData = {};
+    }
+
+    return AppInterfacesAdapter.articleToHeader(articleData);
+  }
+
+  protected backToArticlesList(): void {
+    this.router.navigate(['/blog']);
   }
 }
