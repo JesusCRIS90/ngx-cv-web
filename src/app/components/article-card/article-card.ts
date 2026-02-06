f2fa1ddb3bf0c675bc2903dcfa2e2ecd888ffc83
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2026-02-06 20:27:34 +0100

    [ADD] Enhance article card styling with improved layout and typography, and update blog list page layout

--- src/app/components/article-card/article-card.ts
+++ src/app/components/article-card/article-card.ts
@@ -3,6 +3,8 @@ import { RouterLink } from "@angular/router";
 
 import {
   GridLayout1DComponent as GridLay,
+  HorizontalLayoutComponent as HoriLay,
+  POLICY_POSITION as policyPos
 } from '@beexy/ngx-layouts'
 
 import { ArticleCard } from '../../interfaces'
@@ -10,11 +12,12 @@ import { ArticleCard } from '../../interfaces'
 
 @Component({
   selector: 'article-card',
-  imports: [GridLay, RouterLink],
+  imports: [GridLay, HoriLay, RouterLink],
   templateUrl: './article-card.html',
   styleUrl: './article-card.css',
   encapsulation: ViewEncapsulation.None,
 })
 export class ArticleCardComponent {
+  policyPos = policyPos;
   articleCardData = input.required<ArticleCard>();
 }
